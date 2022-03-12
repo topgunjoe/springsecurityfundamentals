2a76cb610f21daac9f702640be31668afb34e42c
topgunjoe <joe.w.wachiuri@gmail.com>
2022-03-12 11:35:22 +0300

    CORS exceptions implemented

--- couponreactclient/src/App.js
+++ couponreactclient/src/App.js
@@ -6,7 +6,7 @@ import {useEffect} from "react";
 function App() {
 
   useEffect(()=>{
-  axios.get('http://localhost:9091/couponapi/coupons/SUPERSALE').then((response) => {
+  axios.get('http://localhost:9091/couponapi/coupons/MINORSAVE').then((response) => {
   document.write("Coupon Code: "+response.data.code+"<br/>")
   document.write("Coupon Discount: "+response.data.discount+"<br/>")
   document.write("Coupon Expiry Date: "+response.data.expDate+"<br/>")
--- couponservice/src/main/java/com/bharath/springcloud/security/config/WebSecurityConfig.java
+++ couponservice/src/main/java/com/bharath/springcloud/security/config/WebSecurityConfig.java
@@ -1,20 +1,14 @@
 package com.bharath.springcloud.security.config;
 
-import java.util.List;
-
+import com.bharath.springcloud.security.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-
-import com.bharath.springcloud.security.UserDetailsServiceImpl;
 
 //@Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -31,8 +25,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 				.mvcMatchers(HttpMethod.GET, "/couponapi/coupons/{code:^[A-Z]*$}", "/index", "/showGetCoupon",
-						"/getCoupon", "/couponDetails")
-				.hasAnyRole("USER", "ADMIN")
+						"/getCoupon", "/couponDetails").permitAll()
+				// .hasAnyRole("USER", "ADMIN")
 				.mvcMatchers(HttpMethod.GET, "/showCreateCoupon", "/createCoupon", "/createResponse").hasRole("ADMIN")
 				.mvcMatchers(HttpMethod.POST, "/getCoupon").hasAnyRole("USER", "ADMIN")
 				.mvcMatchers(HttpMethod.POST, "/couponapi/coupons", "/saveCoupon", "/getCoupon").hasRole("ADMIN")
